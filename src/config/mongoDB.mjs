150110f391768f41c8070cb0700ff6eba13ed5a0
Mohak Jain <mohakj500@gmail.com>
2024-11-09 21:31:39 +0530

    wipeout

--- src/config/mongoDB.mjs
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-function initializeDb() {
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => { console.log("Connected to database!") })
-        .catch((err) => { console.log(err) });
-}
-
-export default initializeDb;
